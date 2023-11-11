2c44e2b633b7157b5a4b7d63733d42894930a68c
Lawrence <lawrenceg563@gmail.com>
2023-11-11 14:26:42 +0530

    created initial mvc structure and established mdb connectivity

--- app/app.js
+++ app/app.js
@@ -1,5 +1,12 @@
+import dotevn from "dotenv";
+dotevn.config();
 import express from "express";
+import dbConnect from "../config/dbConnect.js";
+import userRoutes from "../routes/usersRoutes.js"
 
+//db connection
+dbConnect();
 const app = express();
+app.use("/",userRoutes);
 
 export default app;
