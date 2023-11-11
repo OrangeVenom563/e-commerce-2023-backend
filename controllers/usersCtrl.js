1519fb3dbb91d3ba6ab1292bf17fe9f3e7165bd7
Lawrence <lawrenceg563@gmail.com>
2023-11-11 23:36:59 +0530

    added isLoggedin middle ware for profile route

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js"
 import { getTokenFromHeader } from "../utils/getTokenFromHeader.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 //@desc Register User
 //@route POST /api/v1/users/register
@@ -63,7 +64,8 @@ export const loginUserCtrl = asyncHandler(async (req, res) => {
 
 export const getUserProfileCtrl = asyncHandler (async (req,res)=>{
     const token = getTokenFromHeader(req)
-    console.log(token)
+    const verified = verifyToken(token)
+    console.log(verified)
     res.json({
         msg:"Welcome Profile page"
     })
